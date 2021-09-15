c0dbcba5a9f0d49fe53ac14af0449ad1e7efee72
Ramji <90761076+7jipgm@users.noreply.github.com>
2021-09-15 17:50:49 +0530

    Update descriptive.R

--- R/descriptive.R
+++ R/descriptive.R
@@ -6,9 +6,9 @@ ds<-function(x){
   #1 row and 2 coloums
   par(mfrow=c(1,2))
   #histogram
-  hist(x,col=rainbow(30))
+  hist(x,col=rainbow(30), title="Histogram")
   #boxplot
-  boxplot(x, col="blue")
+  boxplot(x, col="blue", title="boxplot")
   par(mfrow=c(1,1))
   #numeric summary
   data.frame(minimum=min(x),maximum=max(x),
