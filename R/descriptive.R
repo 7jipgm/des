c762d4d8c7df459d57f060b0e745e8757bb135bb
Ramji <90761076+7jipgm@users.noreply.github.com>
2021-09-15 17:17:25 +0530

    Add files via upload

---
+++ R/descriptive.R
@@ -0,0 +1,19 @@
+#'create box plot, histogram, t-test and numeric summary
+#'@export
+#'@param x numeric variable
+
+ds<-function(x){
+  #1 row and 2 coloums
+  par(mfrow=c(1,2))
+  #histogram
+  hist(x,col=rainbow(30))
+  #boxplot
+  boxplot(x, col="blue")
+  par(mfrow=c(1,1))
+  #numeric summary
+  data.frame(minimum=min(x),maximum=max(x),
+             median=median(x), sd=sd(x), Range=min(x)-max(x),
+             length=length(x))
+  #t-test
+  t.test=t.test(x)
+}
